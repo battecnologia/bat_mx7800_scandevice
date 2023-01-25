05b3be3995842659061fc66e43c29b8bd9148657
Nilcemar <nilcemar@gmail.com>
2023-01-24 21:06:44 -0300

    Bug fix on imports in java interface

--- android/src/main/java/com/battecnologia/bat_mx7800_scandevice/BatMx7800ScandevicePlugin.java
+++ android/src/main/java/com/battecnologia/bat_mx7800_scandevice/BatMx7800ScandevicePlugin.java
@@ -14,6 +14,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.EventChannel;
 
 /** BatMx7800ScandevicePlugin */
 public class BatMx7800ScandevicePlugin implements FlutterPlugin, MethodCallHandler {
